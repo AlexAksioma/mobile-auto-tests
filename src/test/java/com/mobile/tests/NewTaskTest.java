1905de7a5db7bdd8ca1aeeea00797ce2f7fdd573
alex1khalif <alexkhalifamusic@gmail.com>
2026-06-21 18:51:02 +0300

    created test

---
+++ src/test/java/com/mobile/tests/NewTaskTest.java
@@ -0,0 +1,20 @@
+package com.mobile.tests;
+
+import com.mobile.framework.core.Locator;
+import com.mobile.framework.core.Platform;
+import com.mobile.framework.core.View;
+import com.mobile.framework.pages.HomePage;
+import com.mobile.framework.pages.LoginPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class NewTaskTest {
+    @Test
+    public void testNewTask(){
+        Assert.assertEquals("123", "123");
+    }
+
+
+}
