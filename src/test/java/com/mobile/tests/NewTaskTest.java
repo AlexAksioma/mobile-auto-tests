10d2e9d7eac2c928100e4b470f2ab41f27a5a5f1
pass12345 <tipografia116@gmail.com>
2026-06-21 18:48:42 +0300

    created tests

---
+++ src/test/java/com/mobile/tests/NewTaskTest.java
@@ -0,0 +1,15 @@
+package com.mobile.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class NewTaskTest {
+    @Test
+    public void testNewTask() {
+        Assert.assertEquals("123", "123");
+    }
+
+
+}
+
