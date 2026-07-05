aef854eef3a8a4cc5a7c1db78018f96ea40be1b3
Nina Poliakova <np170577@gmail.com>
2026-07-05 18:56:57 +0300

    testing change

---
+++ src/test/java/com/mobile/tests/NewTaskTest.java
@@ -0,0 +1,11 @@
+package com.mobile.tests;
+
+import org.testng.annotations.Test;
+
+public class NewTaskTest {
+
+    @Test
+    public void newTaskTest(){
+        System.out.println("new task");
+    }
+}
