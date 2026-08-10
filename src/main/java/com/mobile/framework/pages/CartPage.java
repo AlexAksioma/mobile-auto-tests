9401e4c0183d8f5e86b3cadf539c63e780b37908
Efimova Olga <ov.shaikina@gmail.com>
2026-08-10 15:43:24 +0300

    classes: CartItem, ProductItem, ProductCard, Gestures, ProductDetailsPage, ReviewDialog tests: Smoke

---
+++ src/main/java/com/mobile/framework/pages/CartPage.java
@@ -0,0 +1,75 @@
+package com.mobile.framework.pages;
+
+import com.mobile.framework.components.CartItem;
+import com.mobile.framework.core.BasePage;
+import com.mobile.framework.core.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CartPage extends BasePage {
+    public CartPage() {
+        super("//*[contains(@resource-id, 'id/fragment_container')]",
+                "");
+    }
+
+    public View cartContent() {
+        return view("//*[contains(@resource-id, 'id/cartCL')]",
+                "");
+    }
+
+    public View noItemsMessage() {
+        return view("//*[contains(@resource-id, 'id/noItemTitleTV')]",
+                "");
+    }
+
+    private List<CartItem> cartItems() {
+        List<View> itemRoots = views(
+                "//*[contains(@resource-id, 'id/productRV')]/*",
+                ""
+        );
+
+        List<CartItem> cartItems = new ArrayList<>();
+
+        for (View itemRoot : itemRoots) {
+            CartItem cartItem = new CartItem(itemRoot);
+            cartItems.add(cartItem);
+        }
+
+        return cartItems;
+    }
+
+    public List<CartItem> getCartItems() {
+        return cartItems();
+    }
+
+    public View shoppingBtn() {
+        return view("//*[contains(@resource-id, 'id/shoppingBt')]",
+                "");
+    }
+
+    public View title(){
+        return view("//*[contains(@recource-id, 'id/productTV')]",
+                "");
+    }
+
+    public View chekoutBtn(){
+        return view("//*[contains(@resource-id, 'id/cartBt')]",
+                "");
+    }
+
+    public View totalPrice(){
+        return view("//*[contains(@resource-id, 'id/totalPriceTV')]",
+                "");
+    }
+
+    public View totalItems(){
+        return view("//*[contains(@resource-id, 'id/itemsTV')]",
+                "");
+    }
+
+    public void checkout(){
+        chekoutBtn().tap();
+    }
+
+}
---
+++ src/main/java/com/mobile/framework/pages/ProductDetailsPage.java
@@ -0,0 +1,103 @@
+package com.mobile.framework.pages;
+
+import com.mobile.framework.core.BasePage;
+import com.mobile.framework.core.View;
+
+import java.util.List;
+
+public class ProductDetailsPage extends BasePage {
+
+    public ProductDetailsPage() {
+        super(
+                "//*[contains(@resource-id, 'id/fragment_container')]",
+                ""
+        );
+    }
+
+    public View title() {
+        return view(
+                "//*[contains(@resource-id, 'id/productTV')]",
+                ""
+        );
+    }
+
+    public View price() {
+        return view(
+                "//*[contains(@resource-id, 'id/priceTV')]",
+                ""
+        );
+    }
+
+    public View rating() {
+        return view(
+                "//*[contains(@resource-id, 'id/rattingV')]/*",
+                ""
+        );
+    }
+
+    public List<View> colorOptions() {
+        return views(
+                "//*[contains(@resource-id, 'id/colorRV')]/*",
+                ""
+        );
+    }
+
+    public View decreaseQuantityButton() {
+        return view(
+                "//*[contains(@resource-id, 'id/minusIV')]",
+                ""
+        );
+    }
+
+    public View quantity() {
+        return view(
+                "//*[contains(@resource-id, 'id/noTV')]",
+                ""
+        );
+    }
+
+    public View increaseQuantityButton() {
+        return view(
+                "//*[contains(@resource-id, 'id/plusIV')]",
+                ""
+        );
+    }
+
+    public View addToCartButton() {
+        return view(
+                "//*[contains(@resource-id, 'id/cartBt')]",
+                ""
+        );
+    }
+
+    public View productHighlights() {
+        return view(
+                "//*[contains(@resource-id, 'id/productHeightLightsTV')]",
+                ""
+        );
+    }
+
+    public View productHighlightsText() {
+        return view(
+                "//*[contains(@resource-id, 'id/descTV')]",
+                ""
+        );
+    }
+
+    public void addToCart() {
+        addToCartButton().tap();
+    }
+
+    public void increaseQuantity() {
+        increaseQuantityButton().tap();
+    }
+
+    public void decreaseQuantity() {
+        decreaseQuantityButton().tap();
+    }
+
+    public void selectColor(int index) {
+        colorOptions().get(index).tap();
+    }
+
+}
